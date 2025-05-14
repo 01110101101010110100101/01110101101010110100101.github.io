167df49923becd1fef4d86b9a34bd6db8e98b150
01110101101010110100101 <rrtefgdfg@gmail.com>
2025-05-13 22:10:16 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// about blank fixer
+function createGameWindow(url) {
+  var win = window.open();
+  win.document.body.style.margin = "0";
+  win.document.body.style.backgroundColor = "black";
+  var iframe = win.document.createElement('iframe');
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+  iframe.src = url;
+  win.document.body.appendChild(iframe);
+}
+function openGameInNewTab(url) {
+  window.open(url, '_blank');
+}
+
+// abound blank function
+function monkeymart() { createGameWindow("https://limeterracotta.netlify.app/"); }
+function subwaysurf() { createGameWindow("https://enchantedobserver.netlify.app/"); }
+function clust() { createGameWindow("https://skulksensor.netlify.app"); }
+function bit() { createGameWindow("https://oakplank.netlify.app"); }
+function sup() { createGameWindow("https://sm64.netlify.app"); }
+function mo() { createGameWindow("https://lingering-potion-of-the-turtle-master.netlify.app/"); }
+function cut() { createGameWindow("https://cryingobsidian.netlify.app/"); }
+function fruit() { createGameWindow("https://ironaxe.netlify.app/"); }
+function pap() { createGameWindow("https://magentastainedglasspane.netlify.app/"); }
+function vlol() { createGameWindow("https://diamondblock.netlify.app/"); }
+function te() { createGameWindow("https://chickenjockey.netlify.app/"); }
+function stick() { createGameWindow("https://smoothstone.netlify.app/"); }
+function eight() { openGameInNewTab("https://1dot8.netlify.app/"); }
+function five() { openGameInNewTab("https://1dot5.netlify.app/"); }
+function three() { openGameInNewTab("https://1dot3.netlify.app/"); }
+function blank() { createGameWindow("https://launchinaboutblank.netlify.app/"); }
+function nettle() { createGameWindow("https://nettleweb.netlify.app"); }
+function shrimp() { createGameWindow("https://shrimpgames.neocities.org/"); }
+function red() { createGameWindow("https://pokemonredgb.netlify.app/"); }
+function docs() { createGameWindow("https://docs.google.com/document/d/1qh4VDIIYyi37f4nYaUqEiYd-CbI7dN6Bsaoy0X4hsQ0/edit?pli=1&tab=t.0"); }
+function art() { createGameWindow("https://artsyfartsy.penguinensured.com/"); }
+function artt() { createGameWindow("https://art-class-by-maik.vercel.app/"); }
+function au79() { createGameWindow("https://au79.netlify.app"); }
+function nul() { createGameWindow("https://ubg-reborn.vercel.app/index.html"); }
+function proxy() { createGameWindow("https://icantreallythinkthatwell.vercel.app/"); }
+function eagle() { openGameInNewTab("https://eagleslovetosoar.vercel.app/"); }
+function basket() { createGameWindow("https://basketballstars67.pages.dev/"); }
+function neal() { createGameWindow("https://neal.fun/"); }
+function mk() { createGameWindow("https://mk.mgechev.com/"); }
+function ubg() { createGameWindow("https://ubg100.vercel.app/games"); }
+function lil() { createGameWindow("https://mooshroom.netlify.app/"); }
+function bchs() { createGameWindow("https://whydoyouexist.vercel.app"); }
+function sword() { createGameWindow("https://getjiggy.netlify.app"); }
+function launcher() { createGameWindow("https://wolfslauncher.netlify.app"); }
+function t9oss() { createGameWindow("https://t9oss.netlify.app/"); }
+function ttt() { createGameWindow("https://forms.gle/4SaJCfEY5Z8KoBy17"); }
+function dregon() { openGameInNewTab("https://dregonx.netlify.app/"); }
+function onedottwelve() { openGameInNewTab("https://1dot12.netlify.app"); }
+function doge() { createGameWindow("https://chocprompt.vercel.app"); }
+function mc() { createGameWindow("https://yee.pages.dev/"); }
+function stair() { createGameWindow("https://stair-race3-d.vercel.app"); }
+function aqua() { createGameWindow("https://aquaparkio.vercel.app"); }
+function ps() { createGameWindow("https://6813ab0279547a3a94422234--frabjous-praline-3f18dc.netlify.app/"); }
+function gta() { createGameWindow("https://6813ac9e7e29403b49061de1--ephemeral-klepon-728819.netlify.app/"); }
+function slope() { createGameWindow("https://6813ae84bbf6f834f90bef10--inspiring-lollipop-86db13.netlify.app/"); }
+// open in new tab function
+function cattle() { openGameInNewTab("https://crazycatter-35235.web.app/"); }
+function htmleditor() { openGameInNewTab("https://n0tepr0mpt.netlify.app/"); }
+function nv() { openGameInNewTab("https://chocprompt.vercel.app/lessons"); }
